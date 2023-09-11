5e7bb5687b9965e0f2cc581e12c16dc334664996
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2023-09-11 23:13:28 +0300

    add restaurant collection view cell

---
+++ MealApp/MealApp/RestaurantCollectionViewCell/RestaurantCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantCollectionViewCell.swift
+//  MealApp
+//
+//  Created by Şevval Mertoğlu on 11.09.2023.
+//
+
+import UIKit
+
+class RestaurantCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
