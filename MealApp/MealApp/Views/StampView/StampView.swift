faaaacc5f83e0c009b563f9fcc9fc1690a95e935
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2023-09-12 23:21:15 +0300

    add stampview

---
+++ MealApp/MealApp/Views/StampView/StampView.swift
@@ -0,0 +1,35 @@
+//
+//  StampView.swift
+//  MealApp
+//
+//  Created by Şevval Mertoğlu on 11.09.2023.
+//
+
+import UIKit
+
+final class StampView: NibView {
+    
+    @IBOutlet private weak var contentView: UIView!
+    @IBOutlet private weak var imageContainerView: UIView!
+    @IBOutlet private weak var stampImageView: UIImageView!
+    @IBOutlet private weak var stampLabel: UILabel!
+    
+    func configure(title: String,
+                titleColor: UIColor = .white,
+                font: UIFont? = .semibold(12),
+                backgroundColor: UIColor?,
+                cornerRadius: CGFloat = 5,
+                image: UIImage? = nil) {
+        contentView.backgroundColor = backgroundColor
+        contentView.layer.cornerRadius = cornerRadius
+        stampLabel.text = title
+        stampLabel.font = font
+        stampLabel.textColor = titleColor
+        
+        if let image = image {
+            stampImageView.image = image
+        } else {
+            imageContainerView.isHidden = true
+        }
+    }
+}
