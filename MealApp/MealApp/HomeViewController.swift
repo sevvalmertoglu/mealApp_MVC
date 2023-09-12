383e34f04fc35c0f38063fd6adb91b22716df3d7
Sevval Mertoglu <sevvalmertoglu131@gmail.com>
2023-09-13 00:26:34 +0300

    add collectionview data source & delegate

---
+++ MealApp/MealApp/HomeViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  MealApp
+//
+//  Created by Şevval Mertoğlu on 11.09.2023.
+//
+
+import UIKit
+import CoreApi
+
+class HomeViewController: UIViewController {
+    let networkManager: NetworkManager<HomeEndpointItem> = NetworkManager()
+    @IBOutlet private weak var restaurantsCollectionView: UICollectionView!
+    private var widgets: [Widget]?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        prepareCollectionView()
+        fetchWidgets()
+    }
+    
+    private func prepareCollectionView() {
+        restaurantsCollectionView.dataSource = self
+        restaurantsCollectionView.delegate = self
+        
+        restaurantsCollectionView.register(cellType: RestaurantCollectionViewCell.self)
+    }
+    
+    
+    private func fetchWidgets() {
+        networkManager.request(endpoint: .homepage(query: "page=1"), type: HomeResponse.self) { [weak self] result in
+            switch result {
+            case .success(let response):
+                self?.widgets = response.widgets
+                self?.restaurantsCollectionView.reloadData()d
+                print(response)
+                break
+            case .failure(let error):
+                print(error)
+                break
+            }
+        }
+    }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        widgets?.count ?? .zero
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeCell(cellType: RestaurantCollectionViewCell.self, indexPath: indexPath)
+        //cell configure
+        return cell
+    }
+    
+    
+}
+
+extension HomeViewController: UICollectionViewDelegate {
+    
+}
